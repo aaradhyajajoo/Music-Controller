18c85f24d9910a1c8f616e38634b7013000c50aa
aaradhyajajoo <38855824+aaradhyajajoo@users.noreply.github.com>
2022-07-19 19:54:01 -0500

    created a Room model and unique code function

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,20 @@
+from pyexpat import model
 from django.db import models
+import string, random
 
+def generate_unique_code():
+    length = 6
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
+    
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8, unique=True, default="")
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(default=False, null=False)
+    votes_to_skip = models.IntegerField(default=1, null=False)
+    create = models.DateTimeField(auto_now_add=True)
+
