0340dd99c6b1163ac5d5c5d850c96adc574df973
aaradhyajajoo <38855824+aaradhyajajoo@users.noreply.github.com>
2022-07-20 06:06:01 -0500

    figuring out props.match problem

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { useParams } from "react-router-dom";
 
 export default class Room extends Component {
   constructor(props) {
@@ -8,15 +9,34 @@ export default class Room extends Component {
       guestsCanPause: false,
       isHost: false,
     };
-    this.roomCode = this.props.match.params.roomCode;
+    console.log(this.props.match.params.roomCode);
+    // this.roomCode = this.getroomCode();
+    // console.log(this.roomCode);
+    // this.getRoomDetails();
+  }
+//   getroomCode() {
+//     console.log(useParams().roomCode);
+//     return useParams().roomCode;
+//   }
+
+  getRoomDetails() {
+    fetch("/api/get-room" + "?code=" + this.roomCode)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
+      });
   }
   render() {
     return (
       <div>
         <h3>{this.state.roomCode}</h3>
         <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guests Can Pause: {this.state.guestsCanPause}</p>
-        <p>Host: {this.state.isHost}</p>
+        <p>Guests Can Pause: {this.state.guestsCanPause.toString()}</p>
+        <p>Host: {this.state.isHost.toString()}</p>
       </div>
     );
   }
